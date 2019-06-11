021573b26ba13c3d9ae184bc2ab32b80f53b9f89
Leander Vervacke <leandervervacke@hotmail.com>
2019-06-11 12:18:40 +0200

    Update logout.php
    azure logout

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 Session_start();
 Session_destroy();
+// to log out of azure also
+// --> header("Location: https://login.microsoftonline.com/b6e080ea-adb9-4c79-9303-6dcf826fb854/oauth2/logout?post_logout_redirect_uri=https://www.arteveldehogeschool.be/azuredirectory/1/");
+// to log out of the site
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 ?>
