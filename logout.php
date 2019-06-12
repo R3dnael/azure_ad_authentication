ea78b10cee08586673b73647dd1aaae977996117
Leander Vervacke <leandervervacke@hotmail.com>
2019-06-12 09:33:18 +0200

    Update logout.php
    to do: http refferer variable in logout from microsoft

--- logout.php
+++ logout.php
@@ -1,7 +1,9 @@
 <?php
 Session_start();
 Session_destroy();
+//$redirectohome = $_SERVER['HTTP_REFERER'];
 // to log out of azure also
+//to do: implement $redirecttohome in logout variable
 // --> header("Location: https://login.microsoftonline.com/b6e080ea-adb9-4c79-9303-6dcf826fb854/oauth2/logout?post_logout_redirect_uri=https://www.arteveldehogeschool.be/azuredirectory/1/");
 // to log out of the site
 header('Location: ' . $_SERVER['HTTP_REFERER']);
