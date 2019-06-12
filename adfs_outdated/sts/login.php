bfbfb8c04a786d5e329b8e4271947e53ab415522
Leander Vervacke <leandervervacke@hotmail.com>
2019-06-12 10:16:10 +0200

    Add files via upload

---
+++ adfs_outdated/sts/login.php
@@ -0,0 +1,16 @@
+<?php    
+    if(!isset($_SESSION)) {
+        session_start();
+    }
+    include_once("classes/adfsbridge.php");
+    include_once("classes/adfsuserdetails.php");
+    include_once("conf/adfsconf.php"); 
+		
+	if(!isset($_SESSION['AdfsUserDetails'])) 
+	{
+		$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+         	//automatisch doorsturen naar adfs, dit met een redirect naar huidige pg
+         	$adfs = new AdfsBridge();
+         	$adfs->redirectToAdfsSignInUrl(AdfsConf::getInstance(), $actual_link);    
+	}
+?>
